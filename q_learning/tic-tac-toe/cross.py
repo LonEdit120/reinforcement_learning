55573c85b32122eb0dbf3ddd5e721f2118a295e8
bladeath860620 <bladeath860620@gmail.com>
2017-07-31 19:55:41 +0800

    changed file structure + added tic-tac-toe learning

---
+++ q_learning/tic-tac-toe/cross.py
@@ -0,0 +1,79 @@
+discount = 0.9
+learnRate = 0.1
+scoreBoard = []
+preKey = 0
+curKey = 0
+newScore = 0
+def setCurKey(key):
+    curKey = key
+
+def setPreKey(key):
+    preKey = key
+
+def checkExist(key):
+    if not scoreBoard:
+        return True
+    else:
+        for temp in scoreBoard:
+            if temp["key"] == key:
+                return True
+            else:
+                return False
+
+def getScore(key):
+    if key not in scoreBoard:
+        return 0
+    else:
+        for temp in scoreBoard:
+            if temp["key"] == key:
+                return temp["score"]
+
+def update(winner,preKey,curKey):
+    # print('Xs preKey : ' + str(preKey))
+    # print('Xs curKey : ' + str(curKey))
+
+    exist = False
+    for i in range(len(scoreBoard)):
+        if scoreBoard[i]["key"] == curKey:
+            curScore = scoreBoard[i]["score"]
+            exist = True
+            break
+    if (not exist):
+        curScore = 0
+        scoreBoard.append({"key":curKey,"score":0})
+
+    exist = False
+    for j in range(len(scoreBoard)):
+        if scoreBoard[j]["key"] == preKey:
+            preScore = scoreBoard[j]["score"]
+            exist = True
+            break
+    if (not exist):
+        scoreBoard.append({"key":preKey,"score":0})
+        preScore = 0
+        # print('bugged : X preKey ' + str(preKey) + ' not found===========')
+
+    if winner == 'O':
+        scoreBoard[j]["score"] = preScore + learnRate * ((-1) - curScore)
+    if winner == 'X':
+        scoreBoard[j]["score"] = preScore + learnRate * ((1) - curScore)
+    if winner == 'even':
+        scoreBoard[j]["score"] = preScore + learnRate * ((0.3) - curScore)
+    if curScore > preScore:
+        scoreBoard[j]["score"] = preScore + learnRate * (0 + discount * curScore - preScore)
+
+    # print('Xs scoreBoard :')
+    # for temp in scoreBoard:
+    #     print(temp)
+
+def getBoard():
+    print('Xs scoreBoard :')
+    for temp in scoreBoard:
+        print(temp)
+
+def toFile():
+    with open('crossTrainData.txt','w+') as f:
+        f.write('          KEY                                SCORE\n')
+        for temp in scoreBoard:
+            f.write('{0:20d}                  '.format(temp['key']) + str(temp['score'])+'\n')
+    print('Cross train data successfully imported to crossTrainData.txt !')
---
+++ q_learning/tic-tac-toe/train.py
@@ -0,0 +1,204 @@
+import circle
+import cross
+import random
+
+discount = 0.9
+learnRate = 0.1
+jump = 0.2
+trainTimes = 100000
+
+game = (('?','?','?'),('?','?','?'),('?','?','?'))
+tempGame = ()
+
+OpreKey = 0
+OcurKey = 0
+XpreKey = 0
+XcurKey = 0
+
+def showMap(game):
+    for temp in game:
+        print (temp)
+
+def whoWon(game):
+    cnt = 0
+    for i in range(3):
+        cnt = 0
+        for check in game[i]:
+            if check == 'O':
+                cnt += 1
+        if cnt == 3:
+            return 'O'
+    for i in range(3):
+        cnt = 0
+        for temp in game:
+            if temp[i] == 'O':
+                cnt += 1
+        if cnt == 3:
+            return 'O'
+    if game[0][0] == 'O' and game[1][1] == 'O' and game[2][2] == 'O':
+        return 'O'
+    if game[0][2] == 'O' and game[1][1] == 'O' and game[2][0] == 'O':
+        return 'O'
+    #--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    cnt = 0
+    for i in range(3):
+        cnt = 0
+        for check in game[i]:
+            if check == 'X':
+                cnt += 1
+        if cnt == 3:
+            return 'X'
+    for i in range(3):
+        cnt = 0
+        for temp in game:
+            if temp[i] == 'X':
+                cnt += 1
+        if cnt == 3:
+            return 'X'
+    if game[0][0] == 'X' and game[1][1] == 'X' and game[2][2] == 'X':
+        return 'X'
+    if game[0][2] == 'X' and game[1][1] == 'X' and game[2][0] == 'X':
+        return 'X'
+    return 'nobody'
+
+def place(who, loc):
+    gameList=[]
+    for i in game:
+        gameList.append(list(i))
+
+    gameList[int(loc/3)][loc-int(loc/3)*3] = who
+    temp = tuple(tuple(x) for x in gameList)
+    return temp
+
+
+while trainTimes != 0:
+    if trainTimes % 5000 == 0:
+        print('Remaining train times : ' + str(trainTimes) + ' @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
+    # Initialize
+    trainTimes -= 1
+    turn = -1
+    game = (('?','?','?'),('?','?','?'),('?','?','?'))
+    OpreKey = hash(game)
+    while True:
+        turn *= -1
+        tempScore = []
+        choose = []
+        if turn == 1: # CIRCLE
+            # print('circle')
+            for i in range(len(game)):
+                for j in range(len(game[i])):
+                    if game[i][j] == '?':
+                        tempGame = game
+                        tempGame = place('O',3*i+j)
+                        if circle.checkExist(hash(tempGame)):
+                            tempScore.append(circle.getScore(hash(tempGame)))
+                        else:
+                            tempScore.append(0)
+                    else:
+                        tempScore.append(-100)
+            # JUMP Selection
+            if random.random() > jump:
+                for i in range(len(tempScore)):
+                    if tempScore[i] == max(tempScore):
+                        choose.append(i)
+                rand = random.randint(0,len(choose)-1)
+                # print('O placed at ' + str(choose[rand]))
+                game = place('O',choose[rand])
+            else:
+                for i in range(len(tempScore)):
+                    if tempScore[i] >= 0:
+                        choose.append(i)
+                rand = random.randint(0,len(choose)-1)
+                # print('O placed at ' + str(choose[rand]))
+                game = place('O',choose[rand])
+
+            if whoWon(game) == 'O':
+                cross.update(whoWon(game),XpreKey,XcurKey)
+                # print ('Winner : O')
+                # print('============================================================================')
+                # circle.getBoard()
+                # cross.getBoard()
+                break
+            else:
+                cnt = 0
+                for a in game:
+                    for b in a:
+                        if b != '?':
+                            cnt += 1
+                if cnt == 9:
+                    circle.update('even',OpreKey,OcurKey)
+                    # print('Even !')
+                    break
+            OcurKey = hash(game)
+            # showMap(game)
+            circle.update(whoWon(game),OpreKey,OcurKey)
+            OpreKey = OcurKey
+            # print('-----------------------------------------------')
+
+###############################################################################################
+        else: #CROSS
+            if XpreKey == 0:
+                XpreKey = OcurKey
+            # print('Cross')
+            for i in range(len(game)):
+                for j in range(len(game[i])):
+                    if game[i][j] == '?':
+                        tempGame = game
+                        tempGame = place('X',3*i+j)
+                        if cross.checkExist(hash(tempGame)):
+                            tempScore.append(cross.getScore(hash(tempGame)))
+                        else:
+                            tempScore.append(0)
+                    else:
+                        tempScore.append(-100)
+
+            # JUMP Selection
+            if random.random() > jump:
+                for i in range(len(tempScore)):
+                    if tempScore[i] == max(tempScore):
+                        choose.append(i)
+                rand = random.randint(0,len(choose)-1)
+                # print('X placed at ' + str(choose[rand]))
+                game = place('X',choose[rand])
+            else:
+                for i in range(len(tempScore)):
+                    if tempScore[i] >= 0:
+                        choose.append(i)
+                rand = random.randint(0,len(choose)-1)
+                # print('X placed at ' + str(choose[rand]))
+                game = place('X',choose[rand])
+
+            if whoWon(game) == 'X':
+                circle.update(whoWon(game),OpreKey,OcurKey)
+                # print ('Winner : X')
+                # print('============================================================================')
+                # circle.getBoard()
+                # cross.getBoard()
+                break
+            else:
+                cnt = 0
+                for a in game:
+                    for b in a:
+                        if b != '?':
+                            cnt += 1
+                if cnt == 9:
+                    cross.update('even',OpreKey,OcurKey)
+                    # print('Even !')
+                    break
+            XcurKey = hash(game)
+            # showMap(game)
+            cross.update(whoWon(game),XpreKey,XcurKey)
+            XpreKey = XcurKey
+            # print('----------------------------------------------------------------------------------------------')
+circle.toFile()
+cross.toFile()
+# USER VS AI
+'''
+while True:
+    user = input('Please choose which side you want to be : \n1. circle\n2. cross\n')
+    if user == 1:
+        print('You just ')
+    elif user == 2:
+    else:
+        print('Input 1 or 2 for circle or cross')
+'''
