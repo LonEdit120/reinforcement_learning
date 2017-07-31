14a1e15219dfc5f766772f8606a3df29c8ab0331
bladeath860620 <bladeath860620@gmail.com>
2017-07-31 21:01:33 +0800

    function working as intended, user interface not added yet

--- q_learning/tic-tac-toe/train.py
+++ q_learning/tic-tac-toe/train.py
@@ -2,18 +2,8 @@ import circle
 import cross
 import random
 
-discount = 0.9
-learnRate = 0.1
 jump = 0.2
-trainTimes = 100000
-
 game = (('?','?','?'),('?','?','?'),('?','?','?'))
-tempGame = ()
-
-OpreKey = 0
-OcurKey = 0
-XpreKey = 0
-XcurKey = 0
 
 def showMap(game):
     for temp in game:
@@ -61,7 +51,7 @@ def whoWon(game):
         return 'X'
     return 'nobody'
 
-def place(who, loc):
+def place(who, loc, game):
     gameList=[]
     for i in game:
         gameList.append(list(i))
@@ -70,139 +60,135 @@ def place(who, loc):
     temp = tuple(tuple(x) for x in gameList)
     return temp
 
-
-while trainTimes != 0:
-    if trainTimes % 5000 == 0:
-        print('Remaining train times : ' + str(trainTimes))
-    # Initialize
-    trainTimes -= 1
-    turn = -1
+def train(trainTimes):
+    OpreKey = 0
+    OcurKey = 0
+    XpreKey = 0
+    XcurKey = 0
     game = (('?','?','?'),('?','?','?'),('?','?','?'))
-    OpreKey = hash(game)
-    while True:
-        turn *= -1
-        tempScore = []
-        choose = []
-        if turn == 1: # CIRCLE
-            # print('circle')
-            for i in range(len(game)):
-                for j in range(len(game[i])):
-                    if game[i][j] == '?':
-                        tempGame = game
-                        tempGame = place('O',3*i+j)
-                        if circle.checkExist(hash(tempGame)):
-                            tempScore.append(circle.getScore(hash(tempGame)))
+
+    while trainTimes != 0:
+        if trainTimes % 5000 == 0:
+            print('Remaining train times : ' + str(trainTimes))
+        # Initialize
+        trainTimes -= 1
+        turn = -1
+        game = (('?','?','?'),('?','?','?'),('?','?','?'))
+        OpreKey = hash(game)
+        while True:
+            turn *= -1
+            tempScore = []
+            choose = []
+            if turn == 1: # CIRCLE
+                # print('circle')
+                for i in range(len(game)):
+                    for j in range(len(game[i])):
+                        if game[i][j] == '?':
+                            tempGame = game
+                            tempGame = place('O',3*i+j,game)
+                            if circle.checkExist(hash(tempGame)):
+                                tempScore.append(circle.getScore(hash(tempGame)))
+                            else:
+                                tempScore.append(0)
                         else:
-                            tempScore.append(0)
-                    else:
-                        tempScore.append(-100)
-            # JUMP Selection
-            if random.random() > jump:
-                for i in range(len(tempScore)):
-                    if tempScore[i] == max(tempScore):
-                        choose.append(i)
-                rand = random.randint(0,len(choose)-1)
-                # print('O placed at ' + str(choose[rand]))
-                game = place('O',choose[rand])
-            else:
-                for i in range(len(tempScore)):
-                    if tempScore[i] >= 0:
-                        choose.append(i)
-                rand = random.randint(0,len(choose)-1)
-                # print('O placed at ' + str(choose[rand]))
-                game = place('O',choose[rand])
+                            tempScore.append(-100)
+                # JUMP Selection
+                if random.random() > jump:
+                    for i in range(len(tempScore)):
+                        if tempScore[i] == max(tempScore):
+                            choose.append(i)
+                    rand = random.randint(0,len(choose)-1)
+                    # print('O placed at ' + str(choose[rand]))
+                    game = place('O',choose[rand],game)
+                else:
+                    for i in range(len(tempScore)):
+                        if tempScore[i] >= 0:
+                            choose.append(i)
+                    rand = random.randint(0,len(choose)-1)
+                    # print('O placed at ' + str(choose[rand]))
+                    game = place('O',choose[rand],game)
 
-            if whoWon(game) == 'O':
-                circle.update(whoWon(game),OpreKey,OcurKey)
-                cross.update(whoWon(game),XpreKey,XcurKey)
-                # print ('Winner : O')
-                # print('============================================================================')
-                # circle.getBoard()
-                # cross.getBoard()
-                break
-            else:
-                cnt = 0
-                for a in game:
-                    for b in a:
-                        if b != '?':
-                            cnt += 1
-                if cnt == 9:
-                    circle.update('even',OpreKey,OcurKey)
-                    cross.update('even',OpreKey,OcurKey)
-                    # print('Even !')
+                if whoWon(game) == 'O':
+                    circle.update(whoWon(game),OpreKey,OcurKey)
+                    cross.update(whoWon(game),XpreKey,XcurKey)
+                    # print ('Winner : O')
+                    # print('============================================================================')
+                    # circle.getBoard()
+                    # cross.getBoard()
                     break
-            OcurKey = hash(game)
-            # showMap(game)
-            circle.update(whoWon(game),OpreKey,OcurKey)
-            OpreKey = OcurKey
-            # print('-----------------------------------------------')
+                else:
+                    cnt = 0
+                    for a in game:
+                        for b in a:
+                            if b != '?':
+                                cnt += 1
+                    if cnt == 9:
+                        circle.update('even',OpreKey,OcurKey)
+                        cross.update('even',OpreKey,OcurKey)
+                        # print('Even !')
+                        break
+                OcurKey = hash(game)
+                # showMap(game)
+                circle.update(whoWon(game),OpreKey,OcurKey)
+                OpreKey = OcurKey
+                # print('-----------------------------------------------')
 
-###############################################################################################
-        else: #CROSS
-            if XpreKey == 0:
-                XpreKey = OcurKey
-            # print('Cross')
-            for i in range(len(game)):
-                for j in range(len(game[i])):
-                    if game[i][j] == '?':
-                        tempGame = game
-                        tempGame = place('X',3*i+j)
-                        if cross.checkExist(hash(tempGame)):
-                            tempScore.append(cross.getScore(hash(tempGame)))
+    ###############################################################################################
+            else: #CROSS
+                if XpreKey == 0:
+                    XpreKey = OcurKey
+                # print('Cross')
+                for i in range(len(game)):
+                    for j in range(len(game[i])):
+                        if game[i][j] == '?':
+                            tempGame = game
+                            tempGame = place('X',3*i+j,game)
+                            if cross.checkExist(hash(tempGame)):
+                                tempScore.append(cross.getScore(hash(tempGame)))
+                            else:
+                                tempScore.append(0)
                         else:
-                            tempScore.append(0)
-                    else:
-                        tempScore.append(-100)
+                            tempScore.append(-100)
 
-            # JUMP Selection
-            if random.random() > jump:
-                for i in range(len(tempScore)):
-                    if tempScore[i] == max(tempScore):
-                        choose.append(i)
-                rand = random.randint(0,len(choose)-1)
-                # print('X placed at ' + str(choose[rand]))
-                game = place('X',choose[rand])
-            else:
-                for i in range(len(tempScore)):
-                    if tempScore[i] >= 0:
-                        choose.append(i)
-                rand = random.randint(0,len(choose)-1)
-                # print('X placed at ' + str(choose[rand]))
-                game = place('X',choose[rand])
+                # JUMP Selection
+                if random.random() > jump:
+                    for i in range(len(tempScore)):
+                        if tempScore[i] == max(tempScore):
+                            choose.append(i)
+                    rand = random.randint(0,len(choose)-1)
+                    # print('X placed at ' + str(choose[rand]))
+                    game = place('X',choose[rand],game)
+                else:
+                    for i in range(len(tempScore)):
+                        if tempScore[i] >= 0:
+                            choose.append(i)
+                    rand = random.randint(0,len(choose)-1)
+                    # print('X placed at ' + str(choose[rand]))
+                    game = place('X',choose[rand],game)
 
-            if whoWon(game) == 'X':
-                circle.update(whoWon(game),OpreKey,OcurKey)
-                cross.update(whoWon(game),XpreKey,XcurKey)
-                # print ('Winner : X')
-                # print('============================================================================')
-                # circle.getBoard()
-                # cross.getBoard()
-                break
-            else:
-                cnt = 0
-                for a in game:
-                    for b in a:
-                        if b != '?':
-                            cnt += 1
-                if cnt == 9:
-                    circle.update('even',OpreKey,OcurKey)
-                    cross.update('even',OpreKey,OcurKey)
-                    # print('Even !')
+                if whoWon(game) == 'X':
+                    circle.update(whoWon(game),OpreKey,OcurKey)
+                    cross.update(whoWon(game),XpreKey,XcurKey)
+                    # print ('Winner : X')
+                    # print('============================================================================')
+                    # circle.getBoard()
+                    # cross.getBoard()
                     break
-            XcurKey = hash(game)
-            # showMap(game)
-            cross.update(whoWon(game),XpreKey,XcurKey)
-            XpreKey = XcurKey
-            # print('----------------------------------------------------------------------------------------------')
-circle.toFile()
-cross.toFile()
-# USER VS AI
-'''
-while True:
-    user = input('Please choose which side you want to be : \n1. circle\n2. cross\n')
-    if user == 1:
-        print('You just ')
-    elif user == 2:
-    else:
-        print('Input 1 or 2 for circle or cross')
-'''
+                else:
+                    cnt = 0
+                    for a in game:
+                        for b in a:
+                            if b != '?':
+                                cnt += 1
+                    if cnt == 9:
+                        circle.update('even',OpreKey,OcurKey)
+                        cross.update('even',OpreKey,OcurKey)
+                        # print('Even !')
+                        break
+                XcurKey = hash(game)
+                # showMap(game)
+                cross.update(whoWon(game),XpreKey,XcurKey)
+                XpreKey = XcurKey
+                # print('----------------------------------------------------------------------------------------------')
+    circle.toFile()
+    cross.toFile()
